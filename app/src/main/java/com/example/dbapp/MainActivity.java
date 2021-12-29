0cd441f7f22984938a7fbd4ee77fb09f0bf1dbb1
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-12-30 00:05:23 +0500

    Contacts class

--- app/src/main/java/com/example/dbapp/MainActivity.java
+++ app/src/main/java/com/example/dbapp/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.dbapp.Data.DbHandler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DbHandler db = new DbHandler(MainActivity.this);
+
     }
 }
--- app/src/main/java/com/example/dbapp/model/Contacts.java
+++ app/src/main/java/com/example/dbapp/model/Contacts.java
@@ -1,4 +1,51 @@
 package com.example.dbapp.model;
 
 public class Contacts {
+    private int id;
+    private  String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    private  String contactNumber;
+
+    public Contacts(int id, String name, String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public Contacts( String name, String contactNumber) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public Contacts() {
+
+    }
+
+
+
+
 }
