39c9a03948559e1e799fe30fbf87ade00c4f4193
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-12-29 23:56:01 +0500

    Db handler onCreate

--- app/src/main/java/com/example/dbapp/Data/DbHandler.java
+++ app/src/main/java/com/example/dbapp/Data/DbHandler.java
@@ -1,5 +1,31 @@
 package com.example.dbapp.Data;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.dbapp.params.Params;
 
 public class DbHandler extends SQLiteOpenHelper  {
+
+    public DbHandler(Context context) {
+        super(context, Params.DB_NAME, null, Params.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "CREATE TABLE " + Params.TABLE_NAME + "("
+                + Params.KEY_ID + " INTEGER PRIMARY KEY," + Params.KEY_NAME
+                + " TEXT, " + Params.KEY_PHONE + " TEXT" + ")";
+        Log.d("Db Class", "Query being run is : "+ create);
+        db.execSQL(create);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
---
+++ app/src/main/java/com/example/dbapp/model/Contacts.java
@@ -0,0 +1,4 @@
+package com.example.dbapp.model;
+
+public class Contacts {
+}
